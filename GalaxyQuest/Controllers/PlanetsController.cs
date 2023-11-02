b7936285584707e7df18bb622f08de0d722a3378
jkimball54 <jeremyck54@gmail.com>
2023-11-02 10:04:21 -0600

    Cannot get it to work, really close though

--- GalaxyQuest/Controllers/PlanetsController.cs
+++ GalaxyQuest/Controllers/PlanetsController.cs
@@ -1,15 +1,26 @@
-﻿using GalaxyQuest.Services;
+﻿using GalaxyQuest.Models;
+using GalaxyQuest.Services;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections;
 
 namespace GalaxyQuest.Controllers
 {
     public class PlanetsController : Controller
     {
-        public IActionResult Index()
+        private readonly ISWAPIService _SWAPIService;
+
+        public PlanetsController(ISWAPIService SWAPIService)
+        {
+            _SWAPIService = SWAPIService;
+        }
+
+        public async Task<IActionResult> Index()
         {
             var milkyWayPlanets = MilkyWayGalaxy.Planets;
-            
-            
+            List<PlanetModel> swPlanets = new List<PlanetModel>();
+            swPlanets = await _SWAPIService.GetPlanets();
+            ViewData["swPlanets"] = swPlanets;
+
             return View(milkyWayPlanets);
         }
     }
---
+++ GalaxyQuest/Models/PlanetModel.cs
@@ -0,0 +1,20 @@
+﻿namespace GalaxyQuest.Models
+{
+    public class PlanetModel
+    {
+        public string Name { get; set; }
+        public string Diameter { get; set; }
+        public string RotationPeriod { get; set; }
+        public string OrbitalPeriod { get; set; }
+        public string Gravity { get; set; }
+        public string Population { get; set; }
+        public string Climate { get; set; }
+        public string Terrain { get; set; }
+        public string SurfaceWater { get; set; }
+        public ICollection<string> Residents { get; set; }
+        public ICollection<string> Films { get; set; }
+        public string Url { get; set; }
+        public string Created { get; set; }
+        public string Edited { get; set; }
+    }
+}
